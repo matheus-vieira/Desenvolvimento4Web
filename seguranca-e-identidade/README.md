aef198be5d6d1601c9cd6f7a67ce591012b130d4
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2018-04-05 11:39:56 +0000

    adiconando seguraça e identidade

---
+++ seguranca-e-identidade/README.md
@@ -0,0 +1,13 @@
+# Segurança e Identidade
+
+Cada dia mais devemos nos preocupar com a segurança de aplicações, seja ela web, api, desktop ou serviço.
+
+Devemos manter a confidencialidade dos dados de nossos usuários e ao mesmo tempo temos que manter nossos sistemas livres de ataques.
+
+> Itens que devemos nos assegurar
+>
+> * SQL Injection [Little Bobby Tables](http://bobby-tables.com/)
+> * [XSRF (ataque cross-domain)](https://docs.microsoft.com/pt-br/aspnet/core/security/anti-request-forgery)
+> * Acesso ao sistema de forma segura (login e senha ou login social)
+> * Resetar senha ou adicionar fluxos de autenticação de multi fator
+
---
+++ seguranca-e-identidade/source/ToDo/TodoMvc/Controllers/ToDoController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoMvc.Services;
+using TodoMvc.Models.View;
+using TodoMvc.Models;
+
+namespace TodoMvc.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ITodoItemService _todoItemsService;
+
+        public ToDoController(ITodoItemService todoItemsService)
+        {
+            _todoItemsService = todoItemsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Acessar os dados
+            var todoItems = await _todoItemsService
+                .GetIncompleteItemsAsync();
+            // Montar uma Model
+            var viewModel = new ToDoViewModel
+            {
+                Items = todoItems
+            };
+            // Retornar View
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> AddItem(NewToDoItem newToDoItem)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var successful = await _todoItemsService
+                .AddItemAsync(newToDoItem);
+
+            if (!successful)
+                return BadRequest(new { Error = "Could not add Item"});
+
+            return Ok();
+        }
+
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest();
+
+            var successful = await _todoItemsService
+                .MarkDoneAsync(id);
+
+            return Ok(successful);
+        }
+    }
+}
